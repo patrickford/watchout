dd157b35272653f3c087676e928e65b61e8dd2da
Hack Reactor Students <patrickford@msn.com>
2014-01-17 16:02:30 -0800

    Draw the enemies in an svg element

---
+++ watchout.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+  /****************************
+    Game Options Initialized
+  *****************************/
+
+  var gameOptions = {
+    height: 450,
+    width: 700,
+    nEnemies: 30,
+    padding: 20
+  };
+
+  var gameStats = {
+    score: 0,
+    bestScore: 0
+  };
+
+  /****************************
+    Axes Initialized
+  *****************************/
+
+  var axes = {
+    x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+    y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+  };
+
+  /****************************
+    Game Board
+  *****************************/
+
+  var gameBoard = d3.select('.container').append('svg:svg')
+                    .attr('width', gameOptions.width)
+                    .attr('height', gameOptions.height);
+
+
+  /****************************
+    Enemies Functions
+  *****************************/
+
+  var createEnemies = function() {
+    return _.range(0,gameOptions.nEnemies).map(function(i) {
+      return {
+        id: i,
+        x: Math.random()*100,
+        y: Math.random()*100
+      };
+    });
+  };
+
+  var render = function(enemy_data) {
+
+    var enemies = gameBoard.selectAll('circle.enemy')
+                    .data(enemy_data, function(d) {
+                      return d.id;
+                    });
+
+    enemies.enter().append('svg:circle').attr('class', 'enemy').attr('cx', function(enemy) {
+      return axes.x(enemy.x); // how does this parameter fit into the axes.x call
+    }).attr('cy', function(enemy) {
+      return axes.y(enemy.y);
+    }).attr('r', 50);
+
+    enemies.exit().remove();
+  };
+
+
+  var newEnemyPositions = createEnemies();
+
+    render(newEnemyPositions);
+
+
+
+
+
+
+
+
+
+
+});
+  
