3e4f9277d7de08f90c2264b7e185f6c07afeb843
Patrick Ford <patrickford@msn.com>
2014-01-18 17:50:53 -0800

    Replaced placeholder circles with text

--- tagCloud.js
+++ tagCloud.js
@@ -1,13 +1,33 @@
  /****************************
-  Break string of text into array of words and 
-  strip out all punctuation
+  Strip out punctuation
+
+  Remove common words
+
+  Break string of text into array of words 
 
   Create hash of unique words with frequency count
 *****************************/
+var removeCommonWords = function(text) {
+  var commonWords = ['a','at','the','and','of','in','be','an','here','these',
+    'this','that','from','to','our','on','all','are','under','over',
+    'have','has','had','by','each','every','not','nor','us','i','he',
+    'she','him','her','new','old','can','cannot'];
+
+  for(var i = 0; i < commonWords.length; i++) {
+    var find = ' ' + commonWords[i] + ' ';
+    while(text.indexOf(find) > -1){
+      text = text.replace(find, ' ');
+    }
+  }
+  return text;
+};
+
+
 var wordCount = function(copy) {
   var cleanCopy = copy.toLowerCase().replace(/[,.!:;]/g, "");
+  var keyWords = removeCommonWords(cleanCopy);
   var wordArray = cleanCopy.split(" ");
-  console.dir(wordArray);
+
 	var wordCounts = {};
 	for(var i = 0; i< wordArray.length; i++) {
     var num = wordArray[i];
